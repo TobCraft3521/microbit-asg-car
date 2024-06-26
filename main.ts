7eeded9ad8f740e505f8c3141a823422757950f0
TobCraft3521 <89382328+TobCraft3521@users.noreply.github.com>
2024-06-26 10:24:29 +0200

    basic setup

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
 const MSG = {
-    TEST: 0
+    TEST: 0,
+    CALIBRATE: 1,
+    RUN: 2,
 }
 
 radio.setGroup(120)
 radio.onReceivedMessage(MSG.TEST, () => {
     basic.showIcon(IconNames.Happy)
 })
+
+radio.onReceivedMessage(
+    MSG.CALIBRATE, () => {
+
+    }
+)
