8993defa4f893890ed16eb9955b829b506476c1d
TobCraft3521 <89382328+TobCraft3521@users.noreply.github.com>
2024-06-28 15:36:02 +0200

    finish control endpoints for controller and motor speed settings

--- main.ts
+++ main.ts
@@ -2,15 +2,37 @@ const MSG = {
     TEST: 0,
     CALIBRATE: 1,
     RUN: 2,
-}
+    STOP: 3
 
+}
+let run = false
+let speed = 100
+let lSpeed = 1
+let rSpeed = 1
 radio.setGroup(120)
-radio.onReceivedMessage(MSG.TEST, () => {
-    basic.showIcon(IconNames.Happy)
+
+radio.onReceivedNumber(function (code) {
+    switch (code) {
+        case MSG.TEST:
+            basic.showIcon(IconNames.Confused)
+        case MSG.RUN:
+            run = true
+        case MSG.STOP:
+            run = false
+    }
+    adjustSpeed()
 })
 
-radio.onReceivedMessage(
-    MSG.CALIBRATE, () => {
-
+radio.onReceivedValue((name, val) => {
+    if (name === "lSpeed") {
+        lSpeed = val
+    } else {
+        rSpeed = val
     }
-)
+    adjustSpeed()
+})
+
+const adjustSpeed = () => {
+    servos.P0.run(run && lSpeed * speed)
+    servos.P1.run(run && rSpeed * speed * -1)
+}
